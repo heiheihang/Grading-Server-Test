d64033085ed8a7a7506cfd0300b2d6d3f0592357
Alex Fung <alexfungshunhang@gmail.com>
2020-07-05 19:34:22 +0800

    hello.py Running on Shell!

--- grading_server/submission/urls.py
+++ grading_server/submission/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from . import views
+from submission import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('subs/', views.submissions, name='subs'),
 ]
--- grading_server/submission/views.py
+++ grading_server/submission/views.py
@@ -10,7 +10,12 @@ def index(request):
 
 def submissions(request):
     subs = FileSubssmion.objects.all().filter(graded = False)
+    a = subs[0].file.read()
+    exec(a)
+
 
     context = {
         'subs' : subs
     }
+
+    return render(request, 'submission/subs.html', context = context)
