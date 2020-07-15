970851ea16fb44017be1f939ae8aff04075957e0
Alex Fung <alexfungshunhang@gmail.com>
2020-07-15 10:53:04 +0800

    Fix submission form to individualized directory

--- grading_server/submission/views.py
+++ grading_server/submission/views.py
@@ -15,33 +15,36 @@ from . import my_lib
 @login_required
 def submission_view(request, problem_id):
     problem = get_object_or_404(ProblemModel, id=problem_id)
-    print(request.user)
+    #print(request.user)
     if request.method == 'POST':
-        form = FileSubmissionForm(request.POST)
+        form = FileSubmissionForm(request.POST, request.FILES)
         if form.is_valid():
 
             #input file handling---------------------------------------------------
             file = form.cleaned_data['file']
             fs = FileSystemStorage()
-            time = roundSeconds(datetime.datetime.now())
-            user_id = request.User.id
-            s = str(problem_id) + "_" + str(time) + "_" + str(user_id )
+            time = my_lib.roundSeconds(datetime.datetime.now())
+            t = str(time.year) + '-' + str(time.month) + '-' + str(time.day) + '-' + str(time.hour) + '-' + str(time.minute) + '-' + str(time.second)
+            user_id = request.user.id
+            s = str(user_id) + '/' + str(problem_id) + "_" + str(t) + "_" + str(user_id)+".py"
             filename = fs.save(s,file)    #file name is "problemID_Time_UserID.py"
-
+            print(s)
             #FileSubssmion fields information---------------------------------------
             lang = form.cleaned_data['lang']
             submission_time = time
-            user = request.User
+            user = request.user
             graded = False
 
-            current_submission = FileSubssmion(
-            file = file,
-            submission_time = submission_time,
-            user = user,
-            graded = graded,
-            problem = problem)
+            current_submission = FileSubmission(
+                file = file,
+                submission_time = submission_time,
+                user = user,
+                graded = graded,
+                problem = problem
+            )
+            print(current_submission)
             current_submission.save()
-            return HttpResponseRedirect('/')
+            return HttpResponseRedirect('/submission/')
     else:
         form = FileSubmissionForm()
     context = {
@@ -60,9 +63,14 @@ def problem_submission_index(request):
     return render(request, 'submission/submission_index.html', context)
 
 def submissions(request):
-    subs = FileSubssmion.objects.all().filter(graded=False)
+    subs = FileSubmission.objects.all().filter(graded=False)
     a = subs[0].file.read()
-    exec(a)
+    #exec(a)
+    time = my_lib.roundSeconds(datetime.datetime.now())
+    time = datetime.datetime.now()
+    print(time)
+    t = str(time.year) + '-' + str(time.month) + '-' + str(time.day) + '-' + str(time.hour) + '-' + str(time.minute) + '-' + str(time.second)
+    print(t)
 
     context = {
         'subs': subs
