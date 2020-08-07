b80ad75fb5be1f97ff99e69c1f7f7a8d4a7a79f4
Alex Fung <alexfungshunhang@gmail.com>
2020-08-07 16:43:28 +0800

    adding user info

--- grading_server/home/models.py
+++ grading_server/home/models.py
@@ -1,3 +1,8 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key = True)
+    elo = models.IntegerField()
+    profile_picture = models.ImageField(upload_to = 'users_profile_pic')
--- grading_server/home/views.py
+++ grading_server/home/views.py
@@ -1,7 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import Http404, redirect, get_object_or_404, render
 
 # Create your views here.
+from django.http import HttpResponseRedirect
 
+from django.contrib.auth.decorators import login_required
+from django.core.files.storage import FileSystemStorage
+from problem.models import ProblemModel
+import datetime
+from django.contrib.auth.models import User
+from .models import UserProfile
 
 def index(request):
     return render(request, "index.html")
+
+def user_view(request, user_id):
+    user_object = get_object_or_404(User, user_id)
+    user_profile = user_object.userprofile
+
+    context = {
+        'user_profile' = user_profile,
+        'user' = user_object
+    }
