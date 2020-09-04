9bf21b084d8cf355f908ad11d218fbaffd46af9f
Geoffrey Yeung <yeungchingho123@gmail.com>
2020-09-04 16:15:04 +0800

    cleaning: comments, formatting etc

--- grading_server/submission/models.py
+++ grading_server/submission/models.py
@@ -6,6 +6,7 @@ from submission.choices import *
 from contest.models import ContestModel
 
 def file_submission_name(instance, filename):
+    """create (hopefully) unique and descriptive filename for each submission"""
     # TODO: use my_lib?
     time = instance.submission_time.strftime('%Y-%m-%d-%H-%M-%S')
     user_id = instance.user.id
@@ -26,11 +27,8 @@ class FileSubmission(models.Model):
     lang = models.CharField(max_length=3, choices=LANG_CHOICES)
     submission_time = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    graded = models.BooleanField(default=False)
-    report = models.FileField(upload_to='random/', blank = True, null = True)
-    feedback = models.TextField(max_length=1000, null = True, blank= True)
+
     problem = models.ForeignKey(ProblemModel, on_delete=models.CASCADE, default = None, blank = True, null= True)
-    correct = models.BooleanField(default = False)
     # users can submit to problem after contest ends (for practice)
     # so submission need to check whether it's part of a contest
     # Plus it's easier to find all submissions for a contest this way
@@ -38,7 +36,17 @@ class FileSubmission(models.Model):
         ContestModel, on_delete=models.CASCADE, default=None, blank=True, null=True)
     type = models.CharField(max_length = 10, choices = SUB_CHOICES, default = None, blank = True, null = True)
 
+    # report file that's generated after testing automatically
+    report = models.FileField(upload_to='random/', blank = True, null = True)
+    
+    # manual feedback
+    graded = models.BooleanField(default=False)
+    correct = models.BooleanField(default = False)
+    feedback = models.TextField(max_length=1000, null = True, blank= True)
+
+
     def during_contest(self):
+        """check if submitted after contest ends"""
         if(self.contest != None):
             if(self.submission_time > self.contest.start_time and self.submission_time < self.contest.end_time):
                 self.type = 1
@@ -46,7 +54,9 @@ class FileSubmission(models.Model):
                 self.type = 2
 
     def check_contest(self):
-        if(self.problem.contest != None):
+        """this function is currently not working and not used anywhere
+        find out which contest is this submission part of"""
+        if(self.problem.contests != None):
             self.contest = self.problem.contest
 
 class SubmissionReply(models.Model):
--- grading_server/submission/views.py
+++ grading_server/submission/views.py
@@ -8,31 +8,38 @@ from django.contrib.auth.decorators import login_required
 from django.core.files.storage import FileSystemStorage
 from problem.models import ProblemModel
 from contest.models import ContestModel
-import datetime
 
 from .process_job import process_job
-
 from . import my_lib
 
+from datetime import datetime
 import django_rq
+
 
 @login_required
 def submission_view(request, problem_id):
+    """submission page
+    GET: serves webpage for user to submit a submission to a problem
+    POST: handles submission - check basic validity,
+        fill in implicit values (submit time, if is part of contest),
+        save to database,
+        and queue job to test submission
+    """
     problem = get_object_or_404(ProblemModel, id=problem_id)
     user = request.user
     if not problem.is_visible(user):
         return HttpResponseForbidden()
-    #print(request.user)
+    
     if request.method == 'POST':
         form = FileSubmissionForm(request.POST, request.FILES)
         if form.is_valid():
-
             file = form.cleaned_data['file']
             lang = form.cleaned_data['lang']
-            submission_time = my_lib.roundSeconds(datetime.datetime.now())
-
+            submission_time = my_lib.roundSeconds(datetime.now())
             graded = False
 
+            # find out if this submission is part of a contest
+            # list of all active contests the submitter is contestant of
             contests = [c for c in problem.contests.all() if c.contestants.filter(pk=request.user.pk)]
             contests = [c for c in contests if c.is_active()]
             if len(contests) == 0:
@@ -40,7 +47,8 @@ def submission_view(request, problem_id):
             elif len(contests) == 1:
                 contest = contests[0]
             else:
-                # errrr...?
+                # TODO: errrr...?
+                # submitter is part of 2 active contests that have the same problem... wut
                 contest = contests[0]
 
             current_submission = FileSubmission(
@@ -55,50 +63,68 @@ def submission_view(request, problem_id):
             #print(current_submission)
             current_submission.save()
             current_submission.during_contest()
-            current_submission.check_contest()
+            #current_submission.check_contest()
             current_submission.save()
+
             django_rq.enqueue(process_job, current_submission.pk)
             #process_job(current_submission.pk)
 
             return HttpResponseRedirect('/submission/')
-    else:
+    elif request.method == 'GET':
         form = FileSubmissionForm()
 
-    previous_submissions = FileSubmission.objects.all().filter(user = user, problem = problem).order_by('-submission_time')
-    if(len(previous_submissions) > 3):
-        previous_submissions = previous_submissions[:4]
-    for x in previous_submissions:
-        my_lib.parse_report(x)
-    context = {
-        'form' : form,
-        'problem': problem,
-        'previous_submissions' : previous_submissions
-    }
-
-    return render(request, 'submission/submission_view.html', context)
+        # list of previous submissions user made to the same problem
+        previous_submissions = FileSubmission.objects.all().filter(user = user, problem = problem).order_by('-submission_time')
+        if 'page' in request.GET:
+            page_num = request.GET['page']
+        else:
+            page_num = 0
+        # TODO: constant or configurable?
+        SUBMISSIONS_PER_PAGE = 3
+        previous_submissions = previous_submissions[page_num * SUBMISSIONS_PER_PAGE:(page_num + 1) * SUBMISSIONS_PER_PAGE]
+        
+        for x in previous_submissions:
+            my_lib.parse_report(x)
+        context = {
+            'form' : form,
+            'problem': problem,
+            'previous_submissions' : previous_submissions
+        }
+        return render(request, 'submission/submission_view.html', context)
+    return HttpResponseForbidden(['GET', 'POST'])
 
 def problem_submission_index(request):
+    """list of problems user can submit to,
+    and link to submission page of those problems.
+    accessed by clicking on the "submission" link on navbar"""
     problems = ProblemModel.objects.all()
     problems = [p for p in problems if p.is_visible(request.user)]
-    if(len(problems) > 100):
-        problems = problems[:100]
+
+    if 'page' in request.GET:
+        page_num = request.GET['page']
+    else:
+        page_num = 0
+    # TODO: constant or configurable?
+    PROBLEMS_PER_PAGE = 10
+    problems = problems[page_num * PROBLEMS_PER_PAGE:(page_num + 1) * PROBLEMS_PER_PAGE]
+    
     context = {
         'problems' : problems
     }
     return render(request, 'submission/submission_index.html', context)
 
 def submissions(request):
+    # what does this view do again?
     subs = FileSubmission.objects.all().filter(graded=False)
     a = subs[0].file.read()
     #exec(a)
-    time = my_lib.roundSeconds(datetime.datetime.now())
-    time = datetime.datetime.now()
+    time = my_lib.roundSeconds(datetime.now())
+    time = datetime.now()
     print(time)
-    t = str(time.year) + '-' + str(time.month) + '-' + str(time.day) + '-' + str(time.hour) + '-' + str(time.minute) + '-' + str(time.second)
+    t = '{:%Y-%m-%d-%H-%M-%S}'.format(time)
     print(t)
 
     context = {
         'subs': subs
     }
-
     return render(request, 'submission/subs.html', context=context)
